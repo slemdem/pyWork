ac2e199fa57ae6a6139db412cef917c39963e0a3
진상하 <98368934+slemdem@users.noreply.github.com>
2023-03-30 14:37:07 +0900

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,13 @@
+def rep_char(c, n):
+    print(c*n)
+
+def draw_line_greetings(name) :
+    msg1 = f'Hello {name:s}, \n'
+    msg2 = 'welcome to Seoul. '
+    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
+    rep_char('-',nstr)
+    print(msg1 + msg2)
+    rep_char('-',nstr)
+    
+welcome_name = input('Input his/her name:')
+draw_line_greetings(welcome_name)
