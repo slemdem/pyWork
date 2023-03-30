b47acfcc5e4f6f30cdce3d1e852877d032bc9b8e
진상하 <98368934+slemdem@users.noreply.github.com>
2023-03-30 14:38:05 +0900

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -2,7 +2,7 @@ def rep_char(c, n):
     print(c*n)
 
 def draw_line_greetings(name) :
-    msg1 = f'Hello {name:s}, \n'
+    msg1 = f'Hello {name}, \n'
     msg2 = 'welcome to Seoul. '
     nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
     rep_char('-',nstr)
