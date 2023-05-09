d1303d9a4ca4a883f609a00693ccbead90f07777
진상하 <98368934+slemdem@users.noreply.github.com>
2023-05-09 14:19:34 +0900

    Update hw8.py

--- hw8.py
+++ hw8.py
@@ -7,14 +7,14 @@ def buy(dic):
     print(f'장바구니에 {item} {amount}개가 담겼습니다.', end ='\n\n')
 
 def show(dic):
-    print('>>> 장바구니 보기 :',shopping_bag)
+    print('>>> 장바구니 보기 :',dic)
 
 def find(dic):
     print('\n[검색]')
     serch = input('장바구니에서 확인하고자 하는 상품은? ')
 
-    if serch in shopping_bag :
-        print(f'{serch}은(는) {shopping_bag.get(serch)}개 담겨 있습니다.')
+    if serch in dic :
+        print(f'{serch}은(는) {dic.get(serch)}개 담겨 있습니다.')
     else:
         print(f'장바구니에 {serch}는 없습니다.')
     
